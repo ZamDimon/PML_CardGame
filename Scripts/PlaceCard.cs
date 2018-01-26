0a5bcac0551b8de682009750f33af9a47088cc00
ZamDimon <zamdmytro@gmail.com>
2018-01-26 19:38:38 +0200

    New verson of card placement
    -Now you can move your card when you press LMB.

--- Scripts/PlaceCard.cs
+++ Scripts/PlaceCard.cs
@@ -10,42 +10,72 @@ public class PlaceCard : MonoBehaviour {
 	private Vector3 originPosition; //Start position of a card
 	private Vector3 OnCardPosition; //Position of a card when the mouse is on
 	public float animationSpeed; //Speed of animation
+	public float moveSpeed; //Chasing the mouse speed
+	private float realSpeed;
+	private int currMode = 0;
 
 	public void Start() {
 		//Setting start values
 		originPosition = transform.position; 
 		OnCardPosition = transform.position + new Vector3 (0, 40, 0); 
 		pointToMove = originPosition; 
+		realSpeed = animationSpeed;
 	}
 
-	private void CardAnimation(List <RaycastResult> rayResults) { //Animation of cards method
-		if (rayResults.Count > 0) { 
-			//When we have an array of raycast objects, we need to check if one of them is our object
-			int i = 0; bool FoundCard = false;
-			while (i < rayResults.Count) {
-				if (rayResults [i].gameObject.transform == transform) {
-					FoundCard = true;
-					break; //If we found an object, just stop while operation and set FoundCard = true.
-				}
+	bool IsThisCard(List <RaycastResult> rayResults) { //"Did we find this card" method
+		if (rayResults.Count < 0) return false;
 
-				++i;
-			}
+		//When we have an array of raycast objects, we need to check if one of them is our object
+		int correctResults = 0; bool FoundThisCard = false;
+		foreach (RaycastResult result in rayResults) {
+			if (result.gameObject.transform.tag == "Card")
+				++correctResults;
+			if (result.gameObject.transform == transform)
+				FoundThisCard = true;
+		}
 
-			pointToMove = FoundCard? OnCardPosition : originPosition;   //If we found a card, setting new value for pointToMove, else setting an origin position
-		} 
+		if (correctResults == 1 && FoundThisCard) 
+			return true;
+		
+		return false;
+	} 
+
+	int mode = 0;
+
+	Vector3 MousePoint () {
+		float positionX = Input.mousePosition.x;
+		float positionY = Input.mousePosition.y - transform.GetComponent<RectTransform> ().sizeDelta.y / 2;
+		return new Vector3 (positionX, positionY, 0);
 	}
 		
-	public void Update() {
+	public void FixedUpdate() {
 		//Moving card to a pointToMove position
-		transform.position = Vector3.Lerp(transform.position, pointToMove, Time.deltaTime * animationSpeed); 
+		transform.position = Vector3.Lerp(transform.position, pointToMove, Time.deltaTime * realSpeed); 
 
-		#region UIRaycasts //Raycast for UI
-		PointerEventData pointerData = new PointerEventData (EventSystem.current);
-		pointerData.position = Input.mousePosition;
-		List<RaycastResult> results = new List<RaycastResult> ();
-		EventSystem.current.RaycastAll (pointerData, results);
-		#endregion
+		switch (mode) {
+		case 0:
+			//If we didn't choose the card
+			#region UIRaycasts //Raycast for UI
+			PointerEventData pointerData = new PointerEventData (EventSystem.current);
+			pointerData.position = Input.mousePosition;
+			List<RaycastResult> results = new List<RaycastResult> ();
+			EventSystem.current.RaycastAll (pointerData, results);
+			#endregion
+			
+			pointToMove = (IsThisCard (results) && !Input.GetMouseButton (0)) ? OnCardPosition : originPosition; //Playing animation
+			realSpeed = (IsThisCard (results) && Input.GetMouseButton (0)) ? moveSpeed : realSpeed;
 
-		CardAnimation (results); //Play animation
+			if (IsThisCard (results) && Input.GetMouseButton (0)) 
+				mode = 1;
+			
+			break;
+		case 1: 
+			//If we choose the card
+			pointToMove = MousePoint ();
+			if (Input.GetMouseButtonUp (0)) 
+				mode = 0;
+			
+			break;
+		}
 	}
 }
