c3cdb8ae0020c5973c6be16a950bd38f1fe04172
wsdsw <35849577+wsdsw@users.noreply.github.com>
2018-02-09 23:51:22 +0200

    AudioUpdate
    - menu music system with random tracks

---
+++ menu/scripts/AudioMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMenu : MonoBehaviour {
+	public AudioClip[] MenuMusic;
+	private AudioSource AudioSourse;
+	private int NowTrack;
+	void Start () {
+		AudioSourse = GetComponent<AudioSource> ();
+		NewTrack ();
+	}
+	public void NewTrack(){
+		int Aud = UnityEngine.Random.Range (0, MenuMusic.Length);
+		while (Aud == NowTrack) {
+			Aud = UnityEngine.Random.Range (0, MenuMusic.Length);
+		}
+		Debug.Log ("Track number" + Aud);
+		NowTrack = Aud;
+		AudioSourse.clip =MenuMusic [Aud];
+		AudioSourse.Play ();
+	}
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.R)) {
+			NewTrack ();
+		}
+		if (!AudioSourse.isPlaying) {
+			NewTrack ();
+		}
+	}
+}
